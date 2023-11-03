e7bb5229074b6c014d479d030a4d44349f5d51ce
Wenger-In <97772906+Wenger-In@users.noreply.github.com>
2023-11-03 17:17:20 +0800

    Update synoptic_map_comparison.m

--- synoptic_map_comparison.m
+++ synoptic_map_comparison.m
@@ -14,8 +14,10 @@ lat_res = [30,180, 900,1080,1440];
 red_white_blue = generate_colorbar();
 LineWidth = 2;
 FontSize = 15;
+figure_width = 1600;
+figure_height = 900;
 
-for cr = 2097 : 2097
+for cr = 2100 : 2104
     close all;
 
     %% raw synoptic map
@@ -30,12 +32,10 @@ for cr = 2097 : 2097
             Br_sub = Br_sub / 100; % [G]
             lon_sub = linspace(360,0,73);
             lat_sin = linspace(-14.5/15,14.5/15,30);
-            clim = 10;
         else
             Br_sub = fitsread(obs_file); % [G]
             lon_sub = linspace(0,360,lon_res(i_obs));
             lat_sin = linspace(-1,1,lat_res(i_obs));
-            clim = 30;
         end
         lat_sub = asind(lat_sin);
         Br_raw{i_obs} = Br_sub;
@@ -46,6 +46,11 @@ for cr = 2097 : 2097
     % plot figure
     figure();
     for i_obs = 1 : 5
+        if i_obs == 1
+            clim = 10;
+        else
+            clim = 30;
+        end
         subplot('Position',[0.03+0.33*mod(i_obs-1,3),0.5-0.5*floor((i_obs-0.1)/3),0.3,0.5])
 %         subplot(2,3,i_obs)
         h = pcolor(grid_raw{1, i_obs},grid_raw{2, i_obs},Br_raw{i_obs});
@@ -62,43 +67,58 @@ for cr = 2097 : 2097
     end
 
     annotation('textbox', [0.8, 0.8, 0.2, 0.2], 'String', 'plotted by synoptic\_map\_comparison.m', 'FitBoxToText', 'on');
+    set(gcf,'Position',[10,10,figure_width,figure_height])
 
     if save_or_not == 1
-        save_name = ['CR',num2str(cr),'.png'];
+        save_name = ['CR',num2str(cr),'_raw.png'];
         saveas(gca,[save_dir,save_name]);
     end
 
     %% interpolated synoptic map
-    Br_interp = cell(1,4);
+    % standard grid
+    hmi_lon = grid_raw{1,5};
+    hmi_lat = grid_raw{2,5};
+    std_index = find(abs(sind(hmi_lat))<=14.5/15);
+    std_lon = reshape(hmi_lon(std_index), [], 3600);
+    std_lat = reshape(hmi_lat(std_index), [], 3600);
 
-    % interpolate into HMI grid
-    for i_obs = 1 : 4
-        Br_interp_sub = interp2(grid_raw{1,i_obs}, grid_raw{2,i_obs}, Br_raw{i_obs}, grid_raw{1,5}, grid_raw{2,5}, 'linear');
+    % interpolate
+    Br_interp = cell(1,5);
+    for i_obs = 1 : 5
+        Br_interp_sub = interp2(grid_raw{1,i_obs}, grid_raw{2,i_obs}, Br_raw{i_obs}, std_lon, std_lat, 'linear');
         Br_interp{i_obs} = Br_interp_sub;
     end
 
     % plot figure
-%     figure();
-%     for i_obs = 1 : 4
-%         if i_obs == 1 % WSO
-%             clim = 10;
-%         else
-%             clim = 30;
-%         end
-%         subplot('Position',[0.03+0.33*mod(i_obs-1,3),0.5-0.5*floor((i_obs-0.1)/3),0.3,0.5])
-% %         subplot(2,3,i_obs)
-%         h = pcolor(grid_raw{1,5},grid_raw{2,5},Br_interp{i_obs});
-%         set(h,'LineStyle','none');
-%         shading interp
-%         axis equal
-%         xlim([0 360])
-%         ylim([-90 90])
-% 
-%         colorbar('Location', 'SouthOutside')
-%         colormap(red_white_blue);
-%         title([obs_title{i_obs},' interp','-','CR',num2str(cr)],'FontSize',FontSize)
-%         set(gca,'CLim',[-clim clim],'TickDir','out','XminorTick','on','YminorTick','on','LineWidth',LineWidth,'FontSize',FontSize);
-%     end
+    figure();
+    for i_obs = 1 : 5
+        if i_obs == 1 % WSO
+            clim = 10;
+        else
+            clim = 30;
+        end
+        subplot('Position',[0.03+0.33*mod(i_obs-1,3),0.5-0.5*floor((i_obs-0.1)/3),0.3,0.5])
+%         subplot(2,3,i_obs)
+        h = pcolor(std_lon,std_lat,Br_interp{i_obs});
+        set(h,'LineStyle','none');
+        shading interp
+        axis equal
+        xlim([0 360])
+        ylim([-90 90])
+
+        colorbar('Location', 'SouthOutside')
+        colormap(red_white_blue);
+        title([obs_title{i_obs},' interp','-','CR',num2str(cr)],'FontSize',FontSize)
+        set(gca,'CLim',[-clim clim],'TickDir','out','XminorTick','on','YminorTick','on','LineWidth',LineWidth,'FontSize',FontSize);
+    end
+
+    annotation('textbox', [0.8, 0.8, 0.2, 0.2], 'String', 'plotted by synoptic\_map\_comparison.m', 'FitBoxToText', 'on');
+    set(gcf,'Position',[10,10,figure_width,figure_height])
+
+    if save_or_not == 1
+        save_name = ['CR',num2str(cr),'_interp.png'];
+        saveas(gca,[save_dir,save_name]);
+    end
 
     %% comparison to HMI
     figure();
@@ -106,30 +126,35 @@ for cr = 2097 : 2097
         % plot scatter
         subplot(2,2,i_obs)
         Br_interp_sub = Br_interp{i_obs}(:);
-        Br_hmi = Br_raw{5}(:);
-        scatter(Br_hmi,Br_interp_sub,2,'filled')
-        hold on
+        Br_interp_hmi = Br_interp{5}(:);
+        scatter(Br_interp_hmi,Br_interp_sub,2,'filled')
+        grid on; hold on
 
         % regression
         if i_obs == 3 || i_obs == 4
-            nan_index = isnan(Br_hmi) | isnan(Br_interp_sub);
-            Br_hmi(nan_index) = 0;
+            nan_index = isnan(Br_interp_hmi) | isnan(Br_interp_sub);
+            Br_interp_hmi(nan_index) = 0;
             Br_interp_sub(nan_index) = 0;
-            fit = polyfit(Br_hmi, Br_interp_sub, 1);
-            Br_fit_sub = polyval(fit, Br_hmi);
-            plot(Br_hmi,Br_fit_sub,'r','LineWidth',LineWidth)
-            grid on
+            fit = polyfit(Br_interp_hmi, Br_interp_sub, 1);
+            Br_fit_sub = polyval(fit, Br_interp_hmi);
+            plot(Br_interp_hmi,Br_fit_sub,'r','LineWidth',LineWidth)
             % calculate RMSE
-            residuals = Br_interp_sub - Br_fit_sub;
-            rmse = sqrt(mean(residuals.^2));
-            legend(['RMSE=',num2str(rmse)],['k=',num2str(fit(1))])
+            cc = corrcoef(Br_interp_sub, Br_fit_sub);
+            legend(['cc=',num2str(cc(2))],['k=',num2str(fit(1))],'Location','southeast')
         end
 
         xlabel('HMI [G]')
         ylabel([obs_title{i_obs},'[G]'])
         sgtitle(['CR',num2str(cr)],'FontSize',FontSize)
-
         set(gca,'LineWidth',LineWidth,'FontSize',FontSize)
+    end
+
+    annotation('textbox', [0.8, 0.8, 0.2, 0.2], 'String', 'plotted by synoptic\_map\_comparison.m', 'FitBoxToText', 'on');
+    set(gcf,'Position',[10,10,figure_width,figure_height])
+
+    if save_or_not == 1
+        save_name = ['CR',num2str(cr),'_comparison.png'];
+        saveas(gca,[save_dir,save_name]);
     end
 end
 
