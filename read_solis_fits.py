9c84c630a4edff0a1f05847a06d2bac79f04f478
Wenger-In <97772906+Wenger-In@users.noreply.github.com>
2023-11-03 11:26:59 +0800

    Update read_solis_fits.py

--- read_solis_fits.py
+++ read_solis_fits.py
@@ -1,23 +1,19 @@
 from astropy.io import fits
 import numpy as np
+import os
 
-# 打开.fits文件
-fits_file = fits.open('E:/Research/Data/SOLIS/kbv7g100506t0941c2096_000_int-mas_dim-900.fits')
+solis_dir = 'E:/Research/Data/SOLIS/'
+target_name = '_000_int-mas_dim-900.fits'
 
-# 显示.fits文件的内容
-fits_file.info()
-
-# 访问.fits文件的不同数据单元（HDU）
-# HDU（Header Data Unit）是.fits文件中的数据块
-# 如果你的.fits文件包含多个HDUs，你可以通过索引访问它们
-# 第一个HDU通常是主数据单元（Primary HDU）
-
-# 获取主数据单元（Primary HDU）的数据
-primary_hdu = fits_file[0]
-data = primary_hdu.data
-
-# 关闭.fits文件
-fits_file.close()
-
-# 保存Numpy数组为.fits文件
-fits.writeto('cr2096.fits', data, overwrite=True)
+# 遍历文件夹
+for root, dirs, files in os.walk(solis_dir):
+    for file in files:
+        if file.endswith(target_name):
+            file_path = os.path.join(root, file)
+            fits_file = fits.open(file_path)
+            fits_file.info()
+            primary_hdu = fits_file[0]
+            data = primary_hdu.data
+            fits_file.close()
+            save_name = 'cr' + str(file[17:21]) + '.fits'
+            fits.writeto(solis_dir + save_name, data, overwrite=True)
